6f58d3bd20ef6939d21f31d2bb5f08b2aff368ff
Diego Aguilar <d.aguilaro99@gmail.com>
2023-08-12 18:11:41 -0500

    refactor: sidebar

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,57 +1,67 @@
 import React from 'react';
-import { Box, IconButton, Drawer, DrawerBody, DrawerHeader, DrawerOverlay, DrawerContent, useDisclosure, Link, Flex, DrawerCloseButton } from "@chakra-ui/react";
-import { AiFillCaretRight, AiOutlineCaretLeft } from "react-icons/ai";
+import {
+  Box,
+  IconButton,
+  Drawer,
+  DrawerBody,
+  DrawerOverlay,
+  DrawerContent,
+  useDisclosure,
+  Link,
+  Flex,
+} from '@chakra-ui/react';
+import { AiFillCaretRight, AiOutlineCaretLeft } from 'react-icons/ai';
 
 const Sidebar = () => {
   const { isOpen, onOpen, onClose } = useDisclosure();
-  const btnRef = React.useRef(null); // Initialized with null
+  const btnRef = React.useRef(null);
 
   const links = [
-    { title: "Collectables", url: "/collectables" },
-    { title: "YouTube", url: "https://youtube.com" },
+    { title: 'Collectables', url: '/collectables' },
+    { title: 'Creater Studio', url: '/studio' },
     // add more links as needed
   ];
 
   return (
     <>
-      <IconButton 
-        aria-label="Open Sidebar"
-        icon={isOpen ? <AiOutlineCaretLeft/> : <AiFillCaretRight/>}
-        onClick={isOpen ? onClose : onOpen}
-        color="black"
-        bg="white"
-        minWidth="20px"
-        height="40px"
-        borderRadius="0px"
+      <Box
+        onMouseEnter={onOpen}
+        onMouseLeave={onClose}
         position="fixed"
-        left={isOpen ? "300px" : "0"} // Adjust the left position based on the isOpen state
+        left={0}
         top="50%"
         transform="translateY(-50%)"
-        transition="left 0.5s" // Add a transition effect to the left property
         zIndex="modal"
-        ref={btnRef}
-      />
+      >
+        <IconButton
+          aria-label="Open Sidebar"
+          icon={isOpen ? <></> : <AiFillCaretRight />}
+          color="black"
+          bg="white"
+          minWidth="20px"
+          height="40px"
+          borderRightRadius="5px"
+          borderLeftRadius="0px"
+          transition="left 0.5s"
+          ref={btnRef}
+        />
 
-      <Drawer placement="left" onClose={onClose} isOpen={isOpen} finalFocusRef={btnRef}>
-        <DrawerOverlay>
-          <DrawerContent bg="white">
-            <DrawerBody>
-              <Flex direction="column" align="center" justify="center" h="100%">
-                {links.map((link, index) => (
-                  <Link 
-                    key={index} 
-                    href={link.url} 
-                    color="black" 
-                    _hover={{ color: "gray.300" }}
-                  >
-                    {link.title}
-                  </Link>
-                ))}
-              </Flex>
-            </DrawerBody>
-          </DrawerContent>
-        </DrawerOverlay>
-      </Drawer>
+        <Drawer placement="left" onClose={onClose} isOpen={isOpen} finalFocusRef={btnRef}>
+          <DrawerOverlay>
+            <DrawerContent bg="white">
+              <DrawerBody>
+                <Flex direction="column" align="center" justify="center" h="100%">
+                  {links.map((link, index) => (
+                    <Link key={index} href={link.url} color="black" _hover={{ color: 'gray.300' }}>
+                      {link.title}
+                    </Link>
+                  ))}
+                </Flex>
+              </DrawerBody>
+            </DrawerContent>
+          </DrawerOverlay>
+        </Drawer>
+      </Box>
     </>
   );
 };
